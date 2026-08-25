9e7c5e611a13b118e3b4b77ff6f52bee5e43e09f
sarah <64366044+sarahmorar@users.noreply.github.com>
2026-08-24 23:34:49 -0400

    Add final verse for no more bottles of pop
    Added final verse for when there are no more bottles of pop.

--- bottles_of_pop.cpp
+++ bottles_of_pop.cpp
@@ -9,8 +9,15 @@ int main() {
   // For each bottle of pop we pass around, the next loop will have one less. This repeats until we get to 0. No more pop :(
   for (int i = 99; i > 0; i--) {
     std::cout << i << " bottles of pop on the wall.\n";
+    std::cout << i << " bottles of pop.\n";
     std::cout << "Take one down and pass it around.\n";
     std::cout << i - 1 << " bottles of pop on the wall.\n\n";
   }
+  
+  // Final verse... Here we go again.
+  std::cout << "No more bottles of pop on the wall.\n";
+  std::cout << "No more bottles of pop.\n";
+  std::cout << "Go to the store and buy some more,\n";
+  std::cout << "99 bottles of pop on the wall.\n";
   
 }
